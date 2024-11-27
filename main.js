0e8878730a36e6f98aba8547c38f1adde8b80191
jaxangir <jaxangirr0700@gmail.com>
2024-11-27 19:54:56 +0500

    finished 110%

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let tbody = document.querySelector("tbody");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  fetch("http://localhost:3001/users", {  // Portni bir xil qilish
+  fetch("http://localhost:3000/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,13 +14,15 @@ form.addEventListener("submit", (e) => {
       email: e.target[1].value,
       password: e.target[2].value,
     }),
-  }).then(() => {
-    window.location.reload(); // Ma'lumot qo'shilganda sahifani yangilash
-  }).catch((err) => console.error("Xatolik:", err));
+  })
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((err) => console.error("Xatolik:", err));
 });
 
 (async function () {
-  let response = await fetch("http://localhost:3001/users");
+  let response = await fetch("http://localhost:3000/users");
   let users = await response.json();
 
   users.forEach((user) => {
@@ -32,49 +34,48 @@ form.addEventListener("submit", (e) => {
     let delBtn = document.createElement("button");
     let editBtn = document.createElement("button");
 
-    // Tugmalarni sozlash
     delBtn.textContent = "Delete";
     editBtn.textContent = "Edit";
-    delBtn.classList.add("del-btn")
-    editBtn.classList.add("edit-btn")
+    delBtn.classList.add("del-btn");
+    editBtn.classList.add("edit-btn");
 
-
-    // Jadvalga ma'lumotlarni joylashtirish
     td1.textContent = user.name;
     td2.textContent = user.email;
-    td3.textContent = "***"; // Parolni ko'rsatmaslik
+    td3.textContent = "Password: " + user.password;
 
     tr.append(td1, td2, td3, td4);
-    td4.classList.add("td-btn")
+    td4.classList.add("td-btn");
     td4.append(delBtn, editBtn);
-    tbody.append(tr);
+    tbody.prepend(tr);
 
-    // Delete tugmasi
     delBtn.addEventListener("click", () => {
-      fetch(`http://localhost:3001/users/${user.id}`, {
+      fetch(`http://localhost:3000/users/${user.id}`, {
         method: "DELETE",
-      }).then(() => {
-        tr.remove(); // Jadval qatorini o'chirish
-      }).catch((err) => console.error("Error:", err));
+      })
+        .then(() => {
+          tr.remove();
+        })
+        .catch((err) => console.error("Error:", err));
     });
 
-    // Edit tugmasi
     editBtn.addEventListener("click", () => {
       let name = prompt("Name:", user.name) || user.name;
       let email = prompt("Email:", user.email) || user.email;
-      let password = prompt("Password:", "*****") || user.password;  // Parolni ko'rsatmaslik
+      let password = prompt("Password:", "*****") || user.password;
 
-      fetch(`http://localhost:3001/users/${user.id}`, {
+      fetch(`http://localhost:3000/users/${user.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ name, email, password }),
-      }).then(() => {
-        td1.textContent = name;
-        td2.textContent = email;
-        td3.textContent = "***"; // Parolni ko'rsatmaslik
-      }).catch((err) => console.error("Error:", err));
+      })
+        .then(() => {
+          td1.textContent = name;
+          td2.textContent = email;
+          td3.textContent = "***";
+        })
+        .catch((err) => console.error("Error:", err));
     });
   });
 })();
